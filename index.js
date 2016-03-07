a4e46fa194cb61d89cdf8ebeb35d811cb78ef6fd
Alexander Dukhovnyak <shanhaichik@gmail.com>
2016-03-07 17:27:06 +0300

    Fix

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
 'use strict';
 
-var hasOwn = Object.prototype.hasOwnProperty;
-
 /**
  * Performs equality by iterating through keys on an object and returning false
  * when any key has values which are not strictly equal between the arguments.
  * Returns true when the values of all keys are strictly equal.
  */
 function shallowEqual(objA, objB) {
-    if (objA === objB) {
-        return true;
-    }
-
-    if (typeof objA !== 'object' || objA === null || typeof objB !== 'object' || objB === null) {
-        return false;
-    }
-
-    var keysA = Object.keys(objA);
-    var keysB = Object.keys(objB);
-
-    if (keysA.length !== keysB.length) {
-        return false;
-    }
-
-    // Test for A's keys different from B.
-    var bHasOwnProperty = hasOwn.bind(objB);
-    for (var i = 0; i < keysA.length; i++) {
-        if (!bHasOwnProperty(keysA[i]) || objA[keysA[i]] !== objB[keysA[i]]) {
-            return false;
-        }
-    }
-
+  if (objA === objB) {
     return true;
+  }
+
+  if (typeof objA !== 'object' || objA === null ||
+      typeof objB !== 'object' || objB === null) {
+    return false;
+  }
+
+  var keysA = Object.keys(objA);
+  var keysB = Object.keys(objB);
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  // Test for A's keys different from B.
+  var bHasOwnProperty = Object.prototype.hasOwnProperty.bind(objB);
+  for (var i = 0; i < keysA.length; i++) {
+    if (!bHasOwnProperty(keysA[i]) || objA[keysA[i]] !== objB[keysA[i]]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /**
@@ -44,7 +43,7 @@ function shallowEqual(objA, objB) {
 function shouldComponentUpdate(nextProps, nextState) {
     return (function (inst, nextProps, nextState) {
         return !shallowEqual(inst.props, nextProps) || !shallowEqual(inst.state, nextState);
-    })(this, nextProps, nextState)
+    })(this, nextProps, nextState);
 }
 
 /**
@@ -55,5 +54,3 @@ function shouldComponentUpdate(nextProps, nextState) {
 module.exports = function(component) {
     component.prototype.shouldComponentUpdate = shouldComponentUpdate;
 };
-
-
